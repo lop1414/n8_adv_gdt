7995c47ab1f787442aa4277bb19018641b576669
linyouxin <yllobx@outlook.com>
2022-03-02 10:15:00 +0800

    -

--- app/Sdks/Gdt/Traits/Conversion.php
+++ app/Sdks/Gdt/Traits/Conversion.php
@@ -11,17 +11,17 @@ trait Conversion
      * @param int $pageSize
      * @param array $param
      * @return mixed
-     * 并发获取图片列表
+     * 并发获取转化列表
      */
     public function multiGetConversionList(array $accounts, $page = 1, $pageSize = 10, $param = []){
         $url = $this->getUrl('v1.3/conversions/get');
         if(!isset($param['fields'])){
             $param['fields'] =  [
-                'conversion_id','conversion_name',
-                'optimization_goal','deep_behavior_optimization_goal','deep_worth_optimization_goal',
-                'access_status', 'create_source_type','app_android_channel_package_id', 'promoted_object_id',
-                'access_type','claim_type','feedback_url','self_attributed',
-                'site_set_enable','is_deleted','conversion_scene',
+                'access_status','access_type','app_android_channel_package_id','claim_type',
+                'conversion_id','conversion_name','conversion_scene','create_source_type',
+                'deep_behavior_optimization_goal', 'deep_worth_optimization_goal','feedback_url','is_deleted',
+                'optimization_goal', 'promoted_object_id','self_attributed','site_set_enable',
+                'user_action_set_id','user_action_set_key'
             ];
         }
         $param = $this->filterParam($param);
